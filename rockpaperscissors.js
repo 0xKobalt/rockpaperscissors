67742f16f8a7b397d53e268ee7fec01fe2a47c29
0xKobalt <thien.an.pham@student.uva.nl>
2023-08-31 20:29:01 +0200

    basic ui added

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,94 +1,114 @@
-function getComputerChoice(){
+let humanScore = 0;
+let computerScore = 0;
+let win = false;
+
+function getComputerChoice() {
     let random = Math.random();
-    if(random < 0.3){
+    if (random < 0.3) {
         return "rock";
-    }else if(random < 0.6){
+    } else if (random < 0.6) {
         return "paper";
-    }else{
+    } else {
         return "scissors"
     }
 }
 
-function playRound(playerSelection, computerSelection){
-    if(playerSelection == "rock" && computerSelection == "rock"){
-        return 0;
-    }else if(playerSelection == "rock" && computerSelection == "paper"){
-        return 1;
-    }else if (playerSelection == "rock" && computerSelection == "scissor"){
-        return 2;
-    }else if(playerSelection == "paper" && computerSelection == "rock"){
-        return 3;
-    }else if (playerSelection == "paper" && computerSelection == "paper"){
-        return 4;
-    }else if(playerSelection == "paper" && computerSelection == "scissor"){
-        return 5;
-    }else if (playerSelection == "scissor" && computerSelection == "rock"){
-        return 6;
-    }else if(playerSelection == "scissor" && computerSelection == "paper"){
-        return 7;
-    }else if (playerSelection == "scissor" && computerSelection == "scissor"){
-        return 8;
+function playRound(e) {
+    if (humanScore == 5 || computerScore == 5) {
+        return
     }
-}
 
-function game(){
-    let human = 0;
-    let computer = 0;    
-    let computerSelection = getComputerChoice();
-    let playerSelection = prompt();
+    const computerSelection = getComputerChoice();
+    const playerSelection = e.target.id;
 
-    while(human <= 5 || computer <= 5){
-        switch (playRound(playerSelection, computerSelection)){
-            case 0:
-                console.log("Tie");
-                computerSelection = getComputerChoice();
-                playerSelection = prompt()
-            case 1:
-                console.log("Computer wins!");
-                computerSelection = getComputerChoice();
-                playerSelection = prompt();
-                computer++;
-            case 2:
-                console.log("You win!");
-                computerSelection = getComputerChoice();
-                playerSelection = prompt();
-                human++;
-            case 3:
-                console.log("You win!");
-                computerSelection = getComputerChoice();
-                playerSelection = prompt();
-                human++;
-            case 4:
-                console.log("Tie");
-                computerSelection = getComputerChoice();
-                playerSelection = prompt();
-            case 5:
-                console.log("Computer wins!");
-                computerSelection = getComputerChoice();
-                playerSelection = prompt();
-                computer++;
-            case 6:
-                console.log("Computer wins!");
-                computerSelection = getComputerChoice();
-                playerSelection = prompt();
-                computer++;
-            case 7:
-                console.log("You win!");
-                computerSelection = getComputerChoice();
-                playerSelection = prompt();
-                human++;
-            case 4:
-                console.log("Tie");
-                computerSelection = getComputerChoice();
-                playerSelection = prompt();
+    let roundResult = 0;
+
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        roundResult = 3;
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        roundResult = 2;
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        roundResult = 1;
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        roundResult = 1;
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        roundResult = 3;
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        roundResult = 2;
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        roundResult = 2;
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        roundResult = 1;
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        roundResult = 3;
+    }
+
+    if (roundResult == 1) {
+        humanScore++;
+    } else if (roundResult == 2) {
+        computerScore++;
+    }
+
+    if (humanScore == 5) {
+        if (win == false) {
+            humanWinner();
+        } else {
+            return
         }
+        win = true;
+    } else if (computerScore == 5) {
+        if (win == false) {
+            computerWinner();
+        } else {
+            return
+        }
+        win = true;
     }
 
-    if(human > computer){
-        console.log("You win the game");
-    }else{
-        console.log("Computer wins the game");
-    }
+    updateScoreUi();
+
+    return roundResult;
 }
 
-game();
+function humanWinner() {
+    const container = document.querySelector('#container');
+    const p = document.createElement('p');
+    p.id = "score";
+    p.style.color = 'red';
+    p.textContent = 'You win!';
+    container.appendChild(p);
+}
+
+function computerWinner() {
+    const container = document.querySelector('#container');
+    const p = document.createElement('p');
+    p.id= "score";
+    p.style.color = 'red';
+    p.textContent = 'Computer wins!';
+    container.appendChild(p);
+}
+
+function updateScoreUi() {
+    const human = document.getElementById('human');
+    const computer = document.getElementById('computer');
+    human.textContent = humanScore;
+    computer.textContent = computerScore;
+}
+
+function restartGame(e) {
+    const playerSelection = e.target.id;
+    if (playerSelection == "restart") {
+        humanScore = 0;
+        computerScore = 0;
+        const score = document.querySelector('#score');
+        score.remove();
+        win = false;
+    }
+    updateScoreUi();
+}
+
+window.addEventListener("click", function (e) {
+    const result = playRound(e);
+    const restart = restartGame(e);
+    console.log(result);
+});
